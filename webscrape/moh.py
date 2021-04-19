38e1372263e8b1d0c0bd8c49148e6e608161ced7
Anita Koo Shi Qi  (NCS) <anita.koo@ncs.com.sg>
2021-04-19 16:27:40 +0800

    add write to text file and excel file

--- webscrape/moh.py
+++ webscrape/moh.py
@@ -1,10 +1,17 @@
 from event import Event
+import pandas as pd
 
 class MOH(Event):
 
 	def __init__(self, url):
 		super(MOH, self).__init__(url)
 		self.soup = self.getSoup()
+		self.question_list = ["What is the current DORSCON Level?", 
+		"How many import cases are there?",
+		"How many active cases are there?",
+		"How many people have taken their first dose of vaccination?",
+		"How many people are fully vaccinated?",
+		"How many swap tests has been done?"]
 	
 	def get_dorscon_level(self):
 		results = self.soup.find('div', class_='sfContentBlock')
@@ -55,3 +62,20 @@ class MOH(Event):
 		lweb = lsoup.find("div", class_="left-content")
 		latest_content = lweb.find_all("div", class_="row")
 		return latest_content[1].text
+		
+	def get_data(self, row_count):
+		d_level = self.get_dorscon_level()
+		import_case = self.get_imported_cases()
+		active_case = self.get_active_cases()
+		first_dose = self.get_first_dose_number()
+		full_vac = self.get_full_vaccination_number()
+		swap_test = self.get_number_swap_tested()
+
+		data = {'sn': list(range(row_count, row_count + 6)),
+				'Queston': self.question_list,
+				'Answer': [d_level, import_case, active_case, first_dose, full_vac, swap_test]
+				}
+
+		df = pd.DataFrame(data, columns = ['sn', 'Queston', 'Answer'])
+		df['Short Answer'] = "NA"
+		return df
--- webscrape/webscrape.py
+++ webscrape/webscrape.py
@@ -1,5 +1,26 @@
 from moh import MOH
+import openpyxl
+import pandas
 
 URL = 'https://www.moh.gov.sg/covid-19'
 moh_event = MOH(URL)
 
+
+# write structure data to excel
+wb = openpyxl.load_workbook("COVID_FAQ.xlsx")
+sheet = wb.worksheets[0]
+row_count = sheet.max_row
+data = moh_event.get_data(row_count)
+
+writer = pandas.ExcelWriter('COVID_FAQ.xlsx', engine='openpyxl') 
+writer.book = wb
+writer.sheets = dict((ws.title, ws) for ws in wb.worksheets)
+
+data.to_excel(writer, "FAQ", startrow = row_count, header= False, index = False)
+
+writer.save()
+
+# write non structure data to text
+f = open("data.txt", "a")
+f.write(moh_event.get_latest_article())
+f.close()

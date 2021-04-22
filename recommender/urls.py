4a7705893db77f8a9901ce0c04a2f8990770124e
Sanjeven <sanjeven@gmail.com>
2021-04-22 15:55:23 +0800

    Added chatbot functionality
    Added chatbot functionality

--- recommender/urls.py
+++ recommender/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-
-from recommender.views import RecommenderView, RecommenderThankYouView, RecommenderWelcomeView,RecommenderPositiveView,RecommenderNegativeView,RecommenderAboutView
+from django.conf.urls import url
+from recommender.views import RecommenderView, RecommenderThankYouView, RecommenderWelcomeView,RecommenderPositiveView,RecommenderNegativeView,RecommenderAboutView,ChatBotAppView, ChatBotApiView
 
 
 app_name = "recommender"
@@ -12,6 +12,8 @@ urlpatterns = [
     path('positive/', RecommenderPositiveView.as_view(), name="positive"),
     path('negative/', RecommenderNegativeView.as_view(), name="negative"),
     path('about/', RecommenderAboutView.as_view(), name="about"),
-    path('questionnaire/', RecommenderView.as_view(), name="recommender")
+    path('questionnaire/', RecommenderView.as_view(), name="recommender"),
+    path('chatbot/', ChatBotAppView.as_view(), name="chatbot"),
+    url(r'^api/chatterbot/', ChatBotApiView.as_view(), name='chatterbot'),
 ]
 

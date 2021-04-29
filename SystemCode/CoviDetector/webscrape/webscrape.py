f1f5622f17bc9f41268fb974a9a7486240e6c610
Anita Koo <anitakoo1994@gmail.com>
2021-04-29 17:27:07 +0800

    Change data directory in webscrape python script

--- SystemCode/CoviDetector/webscrape/webscrape.py
+++ SystemCode/CoviDetector/webscrape/webscrape.py
@@ -17,7 +17,7 @@ sheet = wb.worksheets[0]
 row_count = sheet.max_row
 data = moh_event.get_data(row_count)
 
-writer = pandas.ExcelWriter('../chatbot/data/COVID_FAQ.xlsx', engine='openpyxl') 
+writer = pandas.ExcelWriter('../../../Miscellaneous/COVID_FAQ.xlsx', engine='openpyxl') 
 writer.book = wb
 writer.sheets = dict((ws.title, ws) for ws in wb.worksheets)
 
@@ -29,7 +29,7 @@ evt = WM('https://www.worldometers.info/coronavirus/')
 data = evt.get_world_cases()
 
 # write non structure data to text
-f = open("../chatbot/data/data.txt", "a")
+f = open("../../../Miscellaneous/COVID_KNOWLEDGE.txt", "a")
 f.write(moh_event.get_latest_article())
 for d in data:
 	f.write(d)
